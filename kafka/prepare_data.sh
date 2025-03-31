095a7db1ed5ac0e68f589e54b9f476b8b42bf885
NTD <thanhdatnguyen2604@gmail.com>
2025-04-01 01:39:35 +0700

    update docker-compose kafka

--- kafka/prepare_data.sh
+++ kafka/prepare_data.sh
@@ -5,22 +5,23 @@ mkdir -p data/{zookeeper,kafka,eventsim} config
 
 # Tải Million Song Dataset Subset
 echo "Tải Million Song Dataset Subset..."
-wget https://storage.googleapis.com/million-song-dataset-subset/million-song-subset.zip -O data/eventsim/million-song-subset.zip
+wget http://labrosa.ee.columbia.edu/~dpwe/tmp/millionsongsubset.tar.gz -O data/eventsim/millionsongsubset.tar.gz
 
 # Giải nén dữ liệu
 echo "Giải nén dữ liệu..."
-unzip data/eventsim/million-song-subset.zip -d data/eventsim/MillionSongSubset
+tar -xzf data/eventsim/millionsongsubset.tar.gz -C data/eventsim
 
-# Xóa file zip
-rm data/eventsim/million-song-subset.zip
+# Xóa file tar.gz
+rm data/eventsim/millionsongsubset.tar.gz
 
-# Tạo file config.json
+# Tạo file config.json với cấu hình phù hợp với HDF5
 echo "Tạo file config.json..."
 cat > data/eventsim/config.json << EOF
 {
     "users": 1000,
     "songs": 10000,
     "artists": 1000,
+    "h5_data_path": "/data/MillionSongSubset",
     "sessions_per_user": 100,
     "session_length": 3600,
     "session_interval": 86400,
@@ -28,5 +29,9 @@ cat > data/eventsim/config.json << EOF
     "song_interval": 300
 }
 EOF
+
+# Kiểm tra cấu trúc dữ liệu
+echo "Kiểm tra cấu trúc dữ liệu..."
+find data/eventsim/MillionSongSubset -name "*.h5" | head -n 5
 
 echo "Chuẩn bị dữ liệu hoàn tất!" 
